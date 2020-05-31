52ccb2bfb5eb736e6a9594a884eb0c40aaaaf737
HidekiYoshikado <s2010691@u.tsukuba.ac.jp>
2020-05-31 23:48:21 +0900

    途中経過

--- 二分探索.py
+++ 二分探索.py
@@ -1,12 +1,17 @@
-def binarysearch(s):
+def binarysearch():
     l=0
     h=1000000
     while l<h:
         s=(l+h)/2
-        if f(s)==0:
-            return s
-        if f(s)>0:
+        def f(x):
+            return 100-5*x**2/2
+        mid=f(s)
+        if mid==0:
+            break
+        if mid>0:
             l=s
         else:
             h=s
+    print(str(s))
 
+binarysearch()
